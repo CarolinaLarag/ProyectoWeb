fc063366c4f1797e802e6bbec36a1429ac0b42a5
CarolinaLarag <55701648+CarolinaLarag@users.noreply.github.com>
2019-12-15 23:46:01 -0300

    Ultima actualización

--- examen/settings.py
+++ examen/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'America/Santiago'
 
@@ -123,3 +123,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com' # servicio de correo smtp
+EMAIL_HOST_USER = 'carolina.lara.gutierrez@gmail.com' # id de correo electrónico
+EMAIL_HOST_PASSWORD = '20012697-1' #password
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
